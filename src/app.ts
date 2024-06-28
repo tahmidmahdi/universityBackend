4958cfb4bebd056c180aa55ddb7994dfeb6b4d61
tahmidmahdi <tahmid5550@gmail.com>
2024-06-29 00:11:00 +0600

    basic file setup

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import cors from 'cors'
-import express from 'express'
+import express, { Application, Request, Response } from 'express'
 
 // initialize express
-const app = express()
+const app: Application = express()
 
 // basic usages
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
