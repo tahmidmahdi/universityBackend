d1daa01d0d73ed496fd536fff1ac65e5144e258f
Tahmidmahdi <tahmid5550@gmail.com>
2024-07-06 12:30:41 +0600

    validation added for not available ids

--- dist/modules/academicDepartment/academicDepartment.model.js
+++ dist/modules/academicDepartment/academicDepartment.model.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AcademicDepartment = void 0;
 const mongoose_1 = require("mongoose");
@@ -15,5 +24,26 @@ const academicDepartmentSchema = new mongoose_1.Schema({
     },
 }, {
     timestamps: true,
+});
+academicDepartmentSchema.pre('save', function (next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const isDepartmentExist = yield exports.AcademicDepartment.findOne({
+            name: this.name,
+        });
+        if (isDepartmentExist) {
+            throw new Error('Department already exist');
+        }
+        next();
+    });
+});
+academicDepartmentSchema.pre('findOneAndUpdate', function (next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const query = this.getQuery();
+        const isDepartmentExist = yield exports.AcademicDepartment.findOne(query);
+        if (!isDepartmentExist) {
+            throw new Error('Wrong department Id');
+        }
+        next();
+    });
 });
 exports.AcademicDepartment = (0, mongoose_1.model)('academicDepartment', academicDepartmentSchema);
--- src/modules/academicDepartment/academicDepartment.model.ts
+++ src/modules/academicDepartment/academicDepartment.model.ts
@@ -19,6 +19,29 @@ const academicDepartmentSchema = new Schema<IAcademicDepartment>(
   },
 )
 
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  })
+
+  if (isDepartmentExist) {
+    throw new Error('Department already exist')
+  }
+
+  next()
+})
+
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery()
+  const isDepartmentExist = await AcademicDepartment.findOne(query)
+
+  if (!isDepartmentExist) {
+    throw new Error('Wrong department Id')
+  }
+
+  next()
+})
+
 export const AcademicDepartment = model<IAcademicDepartment>(
   'academicDepartment',
   academicDepartmentSchema,
